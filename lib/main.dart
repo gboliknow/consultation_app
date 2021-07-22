eb8a3129b4f98f412ce80525967631e1fb70c192
gboliknow <gboliknow@gmail.com>
2021-07-22 20:17:30 -0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:consultation_ui/screens/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme:
+            GoogleFonts.varelaRoundTextTheme(Theme.of(context).textTheme),
+      ),
+      home: OnboardingScreen(),
+    );
+  }
+}
